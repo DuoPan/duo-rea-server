1ea63a96297d80460d61b638f184828e0b897964
Duo <pd54007@gmail.com>
2021-10-19 23:08:39 +1100

    Add get results api.

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 3001;
-
 const jsonData = require('./db.json');
+
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 
 app.get('/results', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
