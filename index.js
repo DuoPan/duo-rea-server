30d40c64246f424d62211e46912c3477b47bbcc7
Duo <pd54007@gmail.com>
2021-10-20 23:25:35 +1100

    add actions

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const port = 3001;
-const jsonData = require('./db.json');
+const defaultJsonData = require('./db.json');
+let userJsonData = defaultJsonData;
 
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -10,13 +11,68 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.get('/results', (req, res) => {
+app.get('/', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify(jsonData));
+  res.send(JSON.stringify(defaultJsonData));
 });
 
+const addProperty = (id) => {
+  let propertyInResults = userJsonData.results.find(x => x.id === id);
+  // wrong id
+  if (!propertyInResults) {
+    return {message: 'Sorry, we can not find this property.'};
+  }
+
+  let propertyInSaved = userJsonData.saved.find(x => x.id === id);
+  // already added
+  if (propertyInSaved) {
+    return {message: 'You have already saved this property.'};
+  }
+  
+  userJsonData.saved.push(propertyInResults);
+  return {data: userJsonData.saved};
+};
+
+app.get('/add', (req, res) => {
+  let result = {};
+  if (req.query.id) {
+    result = addProperty(req.query.id);
+  } else {
+    // wrong params
+    result = {message: "Miss query parameter id."}
+  }
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(result));
+});
+
+const removeProperty = (id) => {
+  userJsonData.saved = userJsonData.saved.filter(x => x.id !== id);
+  return {data: userJsonData.saved};
+};
+
+app.get('/remove', (req, res) => {
+  let result = {};
+  if (req.query.id) {
+    result = removeProperty(req.query.id);
+  } else {
+    // wrong params
+    result = {message: "Miss query parameter id."}
+  }
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(result));
+});
+
+// todo 
+app.get('/reset', (req, res) => {
+  console.log('jin')
+  userJsonData = defaultJsonData;
+  console.log(userJsonData)
+
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(userJsonData));
+});
 
 // add some logs later
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Thank you for your time.`)
 });
